b6dd9dd996c5b544304418d43a0ae4a1fac961b6
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-08-10 01:25:56 +0600

    update error page

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router';
-import errorImg from '../../assets/errorImg.jpeg';
+import { Link } from 'react-router';
+import errorLotti from '../../assets/lotties/Page Not Found 404.json';
+import Lottie from 'lottie-react';
 
 const ErrorPage = () => {
     return (
-       <div className='mt-26 p-12'>
-            <div className="max-w-md w-full p-6 bg-white rounded-xl
-            flex flex-col items-center justify-center shadow-lg mx-auto">
-                <img className='w-52' src={errorImg} alt="" />
-                <h1 className="text-2xl font-bold text-red-500 mb-4">404 - Page Not Found</h1>
-                <Link 
-                    to="/" 
-                    className="btn btn-primary px-6 py-3 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300"
+        <div className="min-h-screen flex flex-col items-center justify-center bg-base-200 px-6 py-12">
+            <div className="bg-base-100 max-w-lg w-full p-8 rounded-2xl shadow-xl flex flex-col items-center gap-6 border border-secondary/20">
+                
+                {/* Animation */}
+                <div className="max-w-96">
+                    <Lottie animationData={errorLotti} loop={true} />
+                </div>
+
+                {/* Title */}
+                <h1 className="text-3xl font-bold text-error text-center">
+                    404 - Page Not Found
+                </h1>
+
+                {/* Subtitle */}
+                <p className="text-accent text-center leading-relaxed">
+                    Oops! The page you’re looking for doesn’t exist or may have been moved. 
+                    Let’s get you back on track.
+                </p>
+
+                {/* Button */}
+                <Link
+                    to="/"
+                    className="mt-4 bg-primary text-white font-semibold px-6 py-3 rounded-lg shadow-md hover:scale-105 hover:shadow-lg transition-transform duration-300 ease-in-out"
                 >
                     Back to Home
                 </Link>
